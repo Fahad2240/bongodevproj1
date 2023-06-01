05ab5d8f29e0db856c95748eddb6740b8a49ac10
Fahad2240 <fahadish861@gmail.com>
2023-06-02 02:30:42 +0600

    bongodevproj1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      "top-nav": "#282626",
+      "sub-nav": "#353131",
+      "f-color": "#FFF",
+      "main-body-bg": "#808080",
+      "take": "#FFFFFF"
+
+    }
+  },
+  plugins: [],
+}
+
